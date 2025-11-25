b2a55ce11fe38c20c617f928c037349aace0f8e4
Parsa Amini <me@parsaamini.net>
2025-11-24 23:35:54 -0800

    Update build scripts for east-const

--- make.sh
+++ make.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env -S bash -euxo pipefail
 cmake -B build -S . \
     -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_DIR=/opt/homebrew/Cellar/llvm/19.1.7_1/lib/cmake/llvm \
-    -DClang_DIR=/opt/homebrew/Cellar/llvm/19.1.7_1/lib/cmake/clang
+    -DLLVM_DIR=/opt/homebrew/Cellar/llvm/21.1.6/lib/cmake/llvm \
+    -DClang_DIR=/opt/homebrew/Cellar/llvm/21.1.6/lib/cmake/clang
+    # -DCMAKE_CTEST_ARGUMENTS="--output-on-failure" \
+
 cmake --build build
-./build/east-const-enforcer \
-    -fix  /Users/parsa/Repositories/playground/east-const-enforcer/file.cpp \
-    -- \
-    -std=c++17 \
-    -extra-arg=-isystem/opt/homebrew/Cellar/llvm/19.1.7_1/include/c++/v1 \
-    -extra-arg=-isystem/opt/homebrew/Cellar/llvm/19.1.7_1/lib/clang/19/include
+
+# ./build/east-const-enforcer \
+#     -fix \
+#     /Users/parsa/Repositories/playground/east-const-enforcer/file.cpp -- \
+#     -std=c++17 \
+#     -isystem /opt/homebrew/Cellar/llvm/21.1.6/include/c++/v1 \
+#     -isystem /opt/homebrew/Cellar/llvm/21.1.6/lib/clang/19/include
+# cmake --build build --target test
+./build/east-const-enforcer-test
